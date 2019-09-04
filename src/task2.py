2315bdfeeb1b06aa7cfd9e8a018489928648ad28
bernharl <bernharl@student.matnat.uio.no>
2019-09-04 15:20:35 +0200

    Started working on k folds resampling, does not work and is unfinished

--- src/task2.py
+++ src/task2.py
@@ -12,7 +12,7 @@ class ResamplingClass(RegressionClass):
         super().__init__(degree, stddev, step)
         X = super().design_matrix()
         self.X_train, self.X_test, self.z_train, self.z_test = sklms.train_test_split(
-            X, self.z_, train_size=0.66
+            X, self.z_, test_size=0.33
         )
 
     def k_fold(self, k):
@@ -23,13 +23,19 @@ class ResamplingClass(RegressionClass):
         X = self.design_matrix()
         k_fold_data = np.array_split(data, k)
         k_fold_X = np.array_split(X, k)
-        index = np.arange(0, self.n, step = 1)
+        index = np.arange(0, self.n, step = 1, dtype="int")
         index_split = np.array_split(index, k)
-        
-            
-        
-        
-        
+
+        for i in range(k):
+            # print(index_split[i])
+            train_data = k_fold_data[i]
+            test_data = np.delete(k_fold_data, i, axis=0).flatten()
+
+
+
+
+
+
 if __name__ == "__main__":
     np.random.seed(100)
     h = ResamplingClass().k_fold(5)
