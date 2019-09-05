85163b341ed8727df6838cefbf66448f1a4c7199
Lise Nguyen <lise_nguyen@outlook.com>
2019-09-05 15:53:35 +0200

    Added method for LASSO regression

--- src/taskc.py
+++ src/taskc.py
@@ -1,4 +1,4 @@
-from task2 import ResamplingClass
+from taskb import ResamplingClass
 from main import RegressionClass
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
@@ -7,24 +7,34 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
 import sklearn.preprocessing as sklpre
 import sklearn.model_selection as sklms
+import sklearn.linear_model as skllm
 
+
+"""
+I haven't checked the dimensions very throroughly, so please remember 
+to take an extra look at those 
+"""
 
 class PlaceholderName(ResamplingClass):
-    def __init__(self, degree=5, stddev=1, step=0.05, lambd=1):
+    def __init__(self, degree=5, stddev=1, step=0.05, lambd=0.1):
         super().__init__(degree, stddev, step)
         self.lambd = lambd
 
-    def ridge_regression(self):
-        """
-        Just some pseudo-ish code for now before class just to set up the method. 
-        I haven't checked any of the dimensions, so please remember to take
-        an extra look at those 
-        """
-        I = np.identity("fill")
-        self.beta_rigde = np.linalg.solve(
-            np.dot(X.T, X) + self.lambd * I, np.dot(X.T, z)
-        )
 
+    def ridge_regression(self):
+
+        I = np.identity(len(self.X[1]))
+        self.beta = np.linalg.solve(
+            np.dot(self.X.T, self.X) + self.lambd * I, np.dot(self.X.T, self.z_)
+        )
+        self.modeled = True
+
+
+    def lasso_regression(self):
+        self.beta = skllm.Lasso(alpha=self.lambd).fit(self.X, self.z_)
+        self.modeled = True
+     
 
 if __name__ == "__main__":
     c = PlaceholderName().ridge_regression()
+    d = PlaceholderName().lasso_regression()
