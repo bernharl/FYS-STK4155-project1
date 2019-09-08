530ad6271cdc3fb5360f463fc987db3f21d46ca7
Lise Nguyen <lise_nguyen@outlook.com>
2019-09-08 15:03:08 +0200

    Fixed Ridge regression: calculate beta0 as the mean, fixed design matrix X for the method

--- src/taskc.py
+++ src/taskc.py
@@ -10,31 +10,31 @@ import sklearn.model_selection as sklms
 import sklearn.linear_model as skllm
 
 
-"""
-I haven't checked the dimensions very throroughly, so please remember 
-to take an extra look at those 
-"""
 
 class PlaceholderName(ResamplingClass):
     def __init__(self, degree=5, stddev=1, step=0.05, lambd=0.1):
         super().__init__(degree, stddev, step)
         self.lambd = lambd
 
-
     def ridge_regression(self):
-
-        I = np.identity(len(self.X[1]))
-        self.beta = np.linalg.solve(
-            np.dot(self.X.T, self.X) + self.lambd * I, np.dot(self.X.T, self.z_)
+        X = self.X[:,1:]
+        I = np.identity(len(self.X[1])-1)
+        beta = np.zeros(len(self.X[1]))
+        beta[0] = np.mean(self.z_)
+        beta[1:] = np.linalg.solve(
+            np.dot(X.T, X) + self.lambd * I, np.dot(X.T, self.z_)
         )
+        self.beta = beta
         self.modeled = True
-
 
     def lasso_regression(self):
         self.beta = skllm.Lasso(alpha=self.lambd).fit(self.X, self.z_)
         self.modeled = True
-     
+        
+
 
 if __name__ == "__main__":
-    c = PlaceholderName().ridge_regression()
-    d = PlaceholderName().lasso_regression()
+    np.random.seed(100)
+    c = PlaceholderName()
+    c.ridge_regression()
+    print(c.eval_model - c.z_)
