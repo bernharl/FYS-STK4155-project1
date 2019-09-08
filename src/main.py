256bb41a866c10ca1fb77c14fafdd85a4cd48729
bernharl <bernharl@student.matnat.uio.no>
2019-09-08 15:20:39 +0200

    Regression now evaluates the entire meshgrid, not just the diagonal

--- src/main.py
+++ src/main.py
@@ -11,30 +11,37 @@ import sklearn.model_selection as sklms
 
 class RegressionClass:
     def __init__(self, degree=5, stddev=1, step=0.05):
-        self.x = np.arange(0, 1, step)
-        self.y = np.arange(0, 1, step)
+        x = np.arange(0, 1, step)
+        y = np.arange(0, 1, step)
         self.stddev = stddev
+        self.x_meshgrid, self.y_meshgrid = np.meshgrid(x, y)
+        self.z_meshgrid = self.noise_function()
+        self.x = self.x_meshgrid.flatten()
+        self.y = self.y_meshgrid.flatten()
+        self.z_ = self.y_meshgrid.flatten()
         self.n = len(self.x)
         self.degree = degree
         self.modeled = False
-        self.z_ = self.noise_function(self.x, self.y)
 
 
-    def generate_data(self, x, y):
+
+    def generate_data(self):
         """
         Creates the Franke function
         """
+        x = self.x_meshgrid
+        y = self.y_meshgrid
         term1 = 0.75 * np.exp(-(0.25 * (9 * x - 2) ** 2) - 0.25 * ((9 * y - 2) ** 2))
         term2 = 0.75 * np.exp(-((9 * x + 1) ** 2) / 49.0 - 0.1 * (9 * y + 1))
         term3 = 0.5 * np.exp(-(9 * x - 7) ** 2 / 4.0 - 0.25 * ((9 * y - 3) ** 2))
         term4 = -0.2 * np.exp(-(9 * x - 4) ** 2 - (9 * y - 7) ** 2)
         return term1 + term2 + term3 + term4
 
-    def noise_function(self, x, y):
+    def noise_function(self):
         """
         Adds Gaussian noise to the function f,  ~ N(0,stddev)
         """
-        f = self.generate_data(x, y)
+        f = self.generate_data()
         noise = np.random.normal(0, self.stddev, size=f.shape)
         return f + noise
 
@@ -43,12 +50,10 @@ class RegressionClass:
         3D plot of the Franke function
         """
         fig = plt.figure()
-        xx, yy = np.meshgrid(self.x, self.y)
         ax = fig.gca(projection="3d")
-        z = self.noise_function(xx, yy)
         # Plot the surface.
         surf = ax.plot_surface(
-            self.x, self.y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False
+            self.x_meshgrid, self.y_meshgrid, self.z_meshgrid, cmap=cm.coolwarm, linewidth=0, antialiased=False
         )
         # Customize the z axis.
         ax.set_zlim(-0.10, 1.40)
@@ -78,7 +83,7 @@ class RegressionClass:
         XTX = X.T @ X
         XTz = X.T @ self.z_
         beta = np.linalg.solve(XTX, XTz)  # solves XTXbeta = XTz
-        beta_variance = self.stddev ** 2 * np.linalg.pinv(XTX)
+        beta_variance = self.stddev ** 2 * np.linalg.inv(XTX)
         self.beta, self.beta_variance_ = beta, np.diag(beta_variance)
         self.modeled = True
 
@@ -112,11 +117,9 @@ class RegressionClass:
 
 if __name__ == "__main__":
     np.random.seed(100)
-    test = RegressionClass(degree=5, stddev=1, step=0.05)
+    test = RegressionClass(degree=5, stddev=0.1, step=0.05)
+    test.plot_franke()
     test.ordinary_least_squares()
     print(f"MSE {test.mean_squared_error}")
     print(f"R2 score {test.r_squared}")
     print(f"Beta variance {test.beta_variance}")
-    plt.plot(test.eval_model, color="blue")
-    plt.plot(test.z_, color="red")
-    plt.show()
