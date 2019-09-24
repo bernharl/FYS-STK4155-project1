fe3fb258aea1a654e2332948be03f1bad4ff952b
Lise Nguyen <lise_nguyen@outlook.com>
2019-09-24 14:00:45 +0200

    fixed kfold test by using more data points

--- src/test/test_main.py
+++ src/test/test_main.py
@@ -3,18 +3,22 @@ import numpy as np
 import sklearn.preprocessing as sklpre
 import sklearn.model_selection as sklms
 import sklearn.linear_model as skllm
-
 sys.path.append("../")
 from main import OrdinaryLeastSquares, RidgeRegression, LassoRegression
 
+"""
+Test of our OLS and Ridge regression methods from main.py, as well as the k-fold 
+cross validation method. These are all tested using the artificial data from the
+Franke function
+"""
 
 # -------------------- test of OLS regression -----------------------------
 def test_OLS():
     OLS = OrdinaryLeastSquares()
-
+    # scikit-learn method:
     OLS_scikit = skllm.LinearRegression().fit(OLS.X_train, OLS.z_train)
     evalmodel_scikit = OLS_scikit.predict(OLS.X_test)
-
+    # our method:
     OLS.regression_method()
     evalmodel_test = OLS.eval_model
 
@@ -24,10 +28,10 @@ def test_OLS():
 # -------------------- test of Ridge regression --------------------------
 def test_ridge():
     ridge = RidgeRegression()
-
+    # scikit-learn method:
     ridge_scikit = skllm.Ridge(alpha=ridge.lambd).fit(ridge.X_train, ridge.z_train)
     evalmodel_scikit = ridge_scikit.predict(ridge.X_test)
-
+    # our method:
     ridge.regression_method()
     evalmodel_test = ridge.eval_model
 
@@ -37,9 +41,10 @@ def test_ridge():
 # --------------- k-fold cross validation test -----------------------
 def test_kfold_OLS():
     k = 5
-    OLS_test = OrdinaryLeastSquares()
+    # our method:
+    OLS_test = OrdinaryLeastSquares(stddev=0, n_points=100)
     EPE_test = OLS_test.k_fold(k)
-
+    # scikit-learn method:
     kfold = sklms.KFold(n_splits=k, shuffle=True)
     OLS_scikit = skllm.LinearRegression()
     EPE_scikit = np.mean(
@@ -52,6 +57,4 @@ def test_kfold_OLS():
             n_jobs=-1,
         )
     )
-    relative = EPE_test / EPE_scikit
-
-    np.testing.assert_almost_equal(relative, 1, decimal=1)
+    np.testing.assert_allclose(EPE_test, EPE_scikit, rtol=0.01)
