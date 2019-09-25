a2d22565c9302bba1dbd7db1c258d82afade13b6
Lise Nguyen <lise_nguyen@outlook.com>
2019-09-25 15:00:54 +0200

    R2 scores

--- src/r2_scores.py
+++ src/r2_scores.py
@@ -2,61 +2,78 @@ from main import OrdinaryLeastSquares, RidgeRegression, LassoRegression
 
 
 R2_train = []
-R2_test = [] 
+R2_test = []
 
-ols_franke = OrdinaryLeastSquares()
+ols_franke = OrdinaryLeastSquares(stddev=0.1)
 ols_franke.regression_method()
 R2_train.append(ols_franke.r_squared_train)
 R2_test.append(ols_franke.r_squared)
 del ols_franke
 
 ols_terrain = OrdinaryLeastSquares(
+    skip_x_terrain=4,
+    skip_y_terrain=4,
     degree=5,
     terrain_data=True,
     filename="SRTM_data_LakeTanganyika_Africa.tif",
-    path="datafiles/"
+    path="datafiles/",
 )
-ols_terrain.regression_method() 
+ols_terrain.regression_method()
 R2_train.append(ols_terrain.r_squared_train)
 R2_test.append(ols_terrain.r_squared)
 del ols_terrain
-    
-ridge_franke = RidgeRegression() 
-ridge_franke.regression_method() 
+
+ridge_franke = RidgeRegression(stddev=0.1)
+ridge_franke.regression_method()
 R2_train.append(ridge_franke.r_squared_train)
 R2_test.append(ridge_franke.r_squared)
 del ridge_franke
 
 ridge_terrain = RidgeRegression(
+    skip_x_terrain=4,
+    skip_y_terrain=4, 
+    stddev=0.1, 
     degree=5,
     terrain_data=True,
     filename="SRTM_data_LakeTanganyika_Africa.tif",
-    path="datafiles/"
-    )
-ridge_terrain.regression_method() 
+    path="datafiles/",
+)
+ridge_terrain.regression_method()
 R2_train.append(ridge_terrain.r_squared_train)
-R2_test.append(ridge_terrain.r_squared)   
+R2_test.append(ridge_terrain.r_squared)
 del ridge_terrain
 
 
-
-lasso_franke = LassoRegression() 
-lasso_franke.regression_method() 
+lasso_franke = LassoRegression(stddev=0.1, lambd=1e-4)
+lasso_franke.regression_method()
 R2_train.append(lasso_franke.r_squared_train)
 R2_test.append(lasso_franke.r_squared)
 del lasso_franke
 
 lasso_terrain = LassoRegression(
+    skip_x_terrain=4,
+    skip_y_terrain=4,
+    stddev=0.1,
     degree=5,
     terrain_data=True,
     filename="SRTM_data_LakeTanganyika_Africa.tif",
-    path="datafiles/"
-    )
+    path="datafiles/",
+)
 lasso_terrain.regression_method()
 R2_train.append(lasso_terrain.r_squared_train)
-R2_test.append(lasso_terrain.r_squared)   
-del lasso_terrain    
+R2_test.append(lasso_terrain.r_squared)
+del lasso_terrain
 
+
+reg_type = [
+    "OLS Franke",
+    "OLS Terrain",
+    "Ridge Franke",
+    "Ridge Terrain",
+    "Lasso Franke",
+    "Lasso Terrain",
+]
 
 for i in range(len(R2_test)):
-    print(f"R2 training: {R2_train[i]}, R2 test: {R2_test[i]}")
+    print(reg_type[i])
+    print(f"R2 training: {R2_train[i]:.3f}, R2 test: {R2_test[i]:.3f}")
