577d47c104fe038bf6ee0bda26b0811877c58b41
Lise Nguyen <lise_nguyen@outlook.com>
2019-09-25 20:44:00 +0200

    adjusted beta plot size

--- src/beta_variance_ols_plot.py
+++ src/beta_variance_ols_plot.py
@@ -3,31 +3,36 @@ import numpy as np
 from main import OrdinaryLeastSquares
 
 fonts = {
-        "font.family": "serif",
-        "axes.labelsize": 10,
-        "font.size": 10,
-        "legend.fontsize": 10,
-        "xtick.labelsize": 10,
-        "ytick.labelsize": 10,
+    "font.family": "serif",
+    "axes.labelsize": 10,
+    "font.size": 10,
+    "legend.fontsize": 10,
+    "xtick.labelsize": 10,
+    "ytick.labelsize": 10,
 }
 
 plt.rcParams.update(fonts)
 
-ols_franke = OrdinaryLeastSquares(
-    degree=5,
-    stddev=0.1,
-)
+ols_franke = OrdinaryLeastSquares(degree=5, stddev=0.1)
 ols_franke.regression_method()
 x_axis = np.arange(len(ols_franke.beta))
 fig, ax = plt.subplots()
-fig.set_size_inches(  2.9,  1.81134774961)
-ax.errorbar(x_axis, ols_franke.beta, yerr=np.sqrt(ols_franke.beta_variance), fmt=".", capsize=1)
+fig.set_size_inches(2.942, 1.818)
+ax.errorbar(
+    x_axis, ols_franke.beta, yerr=np.sqrt(ols_franke.beta_variance), fmt=".", capsize=1
+)
 ax.set_xlabel(r"$n$")
 ax.set_ylabel(r"$\beta_n$")
 ax.grid()
-ax.set_xticks(range(0, len(ols_franke.beta), 2))
+ax.set_xticks(range(0, len(ols_franke.beta), 4))
+ax.ticklabel_format(style="sci", axis="y", scilimits=(0, 0))
 fig.tight_layout()
-fig.savefig("../doc/figs/beta_variance_ols_Franke.pdf", dpi=1000)
+fig.savefig(
+    "../doc/figs/beta_variance_ols_Franke.pdf",
+    pad_inches=0,
+    bbox_inches="tight",
+    dpi=1000,
+)
 
 
 ols_terrain = OrdinaryLeastSquares(
@@ -41,11 +46,23 @@ ols_terrain = OrdinaryLeastSquares(
 ols_terrain.regression_method()
 x_axis = np.arange(len(ols_terrain.beta))
 fig, ax = plt.subplots()
-fig.set_size_inches(  2.9,  1.81134774961)
-ax.errorbar(x_axis, ols_terrain.beta, yerr=np.sqrt(ols_terrain.beta_variance), fmt=".", capsize=1)
+fig.set_size_inches(2.942, 1.818)
+ax.errorbar(
+    x_axis,
+    ols_terrain.beta,
+    yerr=np.sqrt(ols_terrain.beta_variance),
+    fmt=".",
+    capsize=1,
+)
 ax.set_xlabel(r"$n$")
 ax.set_ylabel(r"$\beta_n$")
 ax.grid()
+ax.set_xticks(range(0, len(ols_terrain.beta), 4))
+ax.ticklabel_format(style="sci", axis="y", scilimits=(0, 0))
 fig.tight_layout()
-ax.set_xticks(range(0, len(ols_terrain.beta), 2))
-fig.savefig("../doc/figs/beta_variance_ols_terrain.pdf", dpi=1000)
+fig.savefig(
+    "../doc/figs/beta_variance_ols_terrain.pdf",
+    pad_inches=0,
+    bbox_inches="tight",
+    dpi=1000,
+)
