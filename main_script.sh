188064e982b9a12874631e36f13420fb570cce83
Lise Nguyen <lise_nguyen@outlook.com>
2019-09-26 20:03:52 +0200

    main_script.sh now also runs test functions

--- main_script.sh
+++ main_script.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 cd src
+
+echo "Run test functions? (y/n)"
+read yn_r
+if [ "$yn_r" == "y" ]
+then 
+  pytest -v
+fi
+
 echo "Print r2 scores? (y/n)"
 read yn_r2
 if [ "$yn_r2" == "y" ]
@@ -8,17 +16,21 @@ then
   python r2_scores.py
 fi
 
-echo "Creating expected prediction error plots"
-python bias_variance_error_terrain.py
-python bias_variance_error_Franke.py
-python beta_variance_ols_plot.py
-python model_plots.py
+echo "Create plots? (y/n)"
+read yn_r3 
+if [ "$yn_r3" == "y" ]
+then
+  python bias_variance_error_terrain.py
+  python bias_variance_error_Franke.py
+  python beta_variance_ols_plot.py
+  python model_plots.py
+fi
 
 echo "Build report? (y/n)"
-read yn
+read yn_r4
 # If y, compile TeX document. The compilation is run many times because
 # bibtex is usually non-cooperative...
-if [ "$yn" == "y" ]
+if [ "$yn_r4" == "y" ]
 then
   cd ../doc/
   pdflatex -synctex=1 -interaction=nonstopmode report_1.tex
