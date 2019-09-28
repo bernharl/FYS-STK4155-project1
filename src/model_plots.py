0743de9fd66b0291ddb2e9eb9c184ba6c4b1a891
bernharl <bernharl@student.matnat.uio.no>
2019-09-28 16:42:11 +0200

    Updated model plots with right degree

--- src/model_plots.py
+++ src/model_plots.py
@@ -13,7 +13,7 @@ del ols_franke
 ols_terrain = OrdinaryLeastSquares(
     skip_x_terrain=4,
     skip_y_terrain=4,
-    degree=5,
+    degree=10,
     terrain_data=True,
     filename="SRTM_data_LakeTanganyika_Africa.tif",
     path="datafiles/",
@@ -30,10 +30,10 @@ del ridge_franke
 
 ridge_terrain = RidgeRegression(
     skip_x_terrain=4,
-    skip_y_terrain=4, 
-    stddev=0.1, 
-    degree=5,
-    lambd=1e-6,
+    skip_y_terrain=4,
+    stddev=0.1,
+    degree=10,
+    lambd=1e-10,
     terrain_data=True,
     filename="SRTM_data_LakeTanganyika_Africa.tif",
     path="datafiles/",
@@ -43,7 +43,7 @@ ridge_terrain.plot_model("Ridge")
 del ridge_terrain
 
 
-# Lasso model plots 
+# Lasso model plots
 lasso_franke = LassoRegression(stddev=0.1, degree=4, lambd=1e-5)
 lasso_franke.regression_method()
 lasso_franke.plot_model("Lasso")
@@ -53,8 +53,8 @@ lasso_terrain = LassoRegression(
     skip_x_terrain=4,
     skip_y_terrain=4,
     stddev=0.1,
-    degree=5,
-    lambd=1e-3, 
+    degree=10,
+    lambd=1e-4,
     terrain_data=True,
     filename="SRTM_data_LakeTanganyika_Africa.tif",
     path="datafiles/",
